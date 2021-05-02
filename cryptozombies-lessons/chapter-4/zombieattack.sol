066ae15d86d96c72b4be570a4c960c360a2d6792
James Hudnall <jimmyhudnall@gmail.com>
2021-05-02 12:49:52 -0400

    Incorporate "random" number generator function

--- cryptozombies-lessons/chapter-4/zombieattack.sol
+++ cryptozombies-lessons/chapter-4/zombieattack.sol
@@ -3,5 +3,10 @@ pragma solidity >=0.5.0 <0.8.5;
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
-
+    // Incorporate "random" number function
+  uint randNonce = 0;
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(now,msg.sender,randNonce))) % _modulus;
+  }
 }
