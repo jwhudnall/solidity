3bbea9d43d44de4a748dace207a92c3efc407797
James Hudnall <jimmyhudnall@gmail.com>
2021-05-02 10:32:42 -0400

    Implement ether fee and levelup function

--- cryptozombies-lessons/chapter-4/zombiehelper.sol
+++ cryptozombies-lessons/chapter-4/zombiehelper.sol
@@ -3,37 +3,53 @@ pragma solidity >=0.5.0 <0.8.5;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+    // Define levelUpFee
+    uint256 levelUpFee = 0.001 ether;
+    
+    modifier aboveLevel(uint256 _level, uint256 _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
 
-  modifier aboveLevel(uint _level, uint _zombieId) {
-    require(zombies[_zombieId].level >= _level);
-    _;
-  }
+    // If proper fee is present, level up zombie
+    function levelUp(uint256 _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }
 
     // Check if zombie level is above 2. If so, allow rename
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    zombies[_zombieId].name = _newName;
-  }
+    function changeName(uint256 _zombieId, string calldata _newName)
+        external
+        aboveLevel(2, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
 
     // Check if above level 20. If so, allow dna change
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    zombies[_zombieId].dna = _newDna;
-  }
-
-  // Function to display zombie collection (by owner)
-  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
-    uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    // Enumerate zombies to check ownership
-    uint counter = 0;
-    for (uint i = 0; i < zombies.length; i++) {
-      if (zombieToOwner[i] == _owner) {
-        result[counter] = i;
-        counter++;
-      }
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(20, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
     }
-    return result; 
-  }
-  
 
+    // Function to display zombie collection (by owner)
+    function getZombiesByOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        // Enumerate zombies to check ownership
+        uint256 counter = 0;
+        for (uint256 i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
 }
