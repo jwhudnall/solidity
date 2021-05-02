d272f8abbe069e8e0922bfa5164963b28e1a9ac9
James Hudnall <jimmyhudnall@gmail.com>
2021-05-02 10:04:42 -0400

    Complete getZombiesByOnwer function

--- cryptozombies-lessons/chapter-3/zombiehelper.sol
+++ cryptozombies-lessons/chapter-3/zombiehelper.sol
@@ -24,7 +24,16 @@ contract ZombieHelper is ZombieFeeding {
   // Function to display zombie collection (by owner)
   function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    // Enumerate zombies to check ownership
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
     return result; 
   }
+  
 
 }
