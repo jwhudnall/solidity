d09fff3ae306d67e2836baa7758feaf1ee93810c
James Hudnall <jimmyhudnall@gmail.com>
2021-05-02 06:31:56 -0400

    Add modifier to require zombies above a given level

---
+++ cryptozombies-lessons/chapter-3/zombiehelper.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 <0.8.5;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    _;
+  }
+
+}
