3b268c94c88700f9a50a0280a896423d5e757987
James Hudnall <jimmyhudnall@gmail.com>
2021-05-02 13:45:19 -0400

    Add initial files for chapter-5

---
+++ cryptozombies-lessons/chapter-5/zombieattack.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.5.0 <0.8.5;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+    uint256 randNonce = 0;
+    uint256 attackVictoryProbability = 70;
+
+    // Incorporate "random" number function
+    function randMod(uint256 _modulus) internal returns (uint256) {
+        randNonce++;
+        return
+            uint256(keccak256(abi.encodePacked(now, msg.sender, randNonce))) %
+            _modulus;
+    }
+
+    // Attack function
+    function attack(uint256 _zombieId, uint256 _targetId)
+        external
+        ownerOf(_zombieId)
+    {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint256 rand = randMod(100);
+        // Battle logic
+        if (rand <= attackVictoryProbability) {
+            myZombie.winCount++;
+            myZombie.level++;
+            enemyZombie.lossCount++;
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
+        }
+    }
+}
---
+++ cryptozombies-lessons/chapter-5/zombiehelper.sol
@@ -0,0 +1,68 @@
+pragma solidity >=0.5.0 <0.8.5;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+    // Define levelUpFee
+    uint256 levelUpFee = 0.001 ether;
+
+    modifier aboveLevel(uint256 _level, uint256 _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+
+    // Create withdraw function
+    function withdraw() external onlyOwner {
+        address payable _owner = address(uint160(owner()));
+        _owner.transfer(address(this).balance);
+    }
+
+    // Owner can modify the levelUpFee
+    function setLevelUpFee(uint256 _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
+    // If proper fee is present, level up zombie
+    function levelUp(uint256 _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }
+
+    // Check if zombie level is above 2. If so, allow rename
+    function changeName(uint256 _zombieId, string calldata _newName)
+        external
+        aboveLevel(2, _zombieId)
+        ownerOf(_zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    // Check if above level 20. If so, allow dna change
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(20, _zombieId)
+        ownerOf(_zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    // Function to display zombie collection (by owner)
+    function getZombiesByOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        // Enumerate zombies to check ownership
+        uint256 counter = 0;
+        for (uint256 i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
+}
