8220b7944846af9cc607f5e736df140f819e470c
James Hudnall <jimmyhudnall@gmail.com>
2021-05-02 18:03:20 -0400

    Add _transfer function to change zombie ownership

--- cryptozombies-lessons/chapter-5/zombieownership.sol
+++ cryptozombies-lessons/chapter-5/zombieownership.sol
@@ -14,6 +14,18 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         return zombieToOwner[_tokenId];
     }
 
+    // Transfer zombie from one owner to another
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) private {
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
     function transferFrom(
         address _from,
         address _to,
